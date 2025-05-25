91a136105c52d0ea70133477d3a06aa2cfe7fee0
Léo Paillé <leo.paille@enseirb-matmeca.fr>
2025-05-25 19:20:54 +0200

    working git add main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
+import distrax
 import gymnasium as gym
-import jax, distrax, optax
-from flax import nnx
+import jax
 import jax.numpy as jnp
-from tqdm import tqdm
 import numpy as np
+import optax
+from flax import nnx
+from tqdm import tqdm
 
 orthogonal = nnx.nn.initializers.orthogonal
 constant = nnx.nn.initializers.constant
 
-BATCH_SIZE = 64
-MAX_STEPS = 256
+BATCH_SIZE = 256
+MAX_STEPS = 2048
 ITERATIONS = 50
 K = 5
 EPS = 0.1
@@ -18,42 +20,92 @@ GAE_LAMBDA = 0.95
 
 
 class Model(nnx.Module):
-    def __init__(self, input_dim, hidden_dim, num_actions, dropout=0.1, rngs: nnx.Rngs=nnx.Rngs(0)):
+    def __init__(
+        self,
+        input_dim,
+        hidden_dim,
+        num_actions,
+        dropout=0.1,
+        rngs: nnx.Rngs = nnx.Rngs(0),
+    ):
         self.policy = nnx.Sequential(
-            nnx.Linear(input_dim, hidden_dim, rngs=rngs, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0)),
+            nnx.Linear(
+                input_dim,
+                hidden_dim,
+                rngs=rngs,
+                kernel_init=orthogonal(np.sqrt(2)),
+                bias_init=constant(0.0),
+            ),
             nnx.Dropout(dropout, rngs=rngs),
             nnx.relu,
-            nnx.Linear(hidden_dim, hidden_dim, rngs=rngs, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0)),
+            nnx.Linear(
+                hidden_dim,
+                hidden_dim,
+                rngs=rngs,
+                kernel_init=orthogonal(np.sqrt(2)),
+                bias_init=constant(0.0),
+            ),
             nnx.Dropout(dropout, rngs=rngs),
             nnx.relu,
-            nnx.Linear(hidden_dim, num_actions, rngs=rngs, kernel_init=orthogonal(0.01), bias_init=constant(0.0))
+            nnx.Linear(
+                hidden_dim,
+                num_actions,
+                rngs=rngs,
+                kernel_init=orthogonal(0.01),
+                bias_init=constant(0.0),
+            ),
         )
         self.value = nnx.Sequential(
-            nnx.Linear(input_dim, hidden_dim, rngs=rngs, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0)),
+            nnx.Linear(
+                input_dim,
+                hidden_dim,
+                rngs=rngs,
+                kernel_init=orthogonal(np.sqrt(2)),
+                bias_init=constant(0.0),
+            ),
             nnx.Dropout(dropout, rngs=rngs),
             nnx.relu,
-            nnx.Linear(hidden_dim, hidden_dim, rngs=rngs, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0)),
+            nnx.Linear(
+                hidden_dim,
+                hidden_dim,
+                rngs=rngs,
+                kernel_init=orthogonal(np.sqrt(2)),
+                bias_init=constant(0.0),
+            ),
             nnx.Dropout(dropout, rngs=rngs),
             nnx.relu,
-            nnx.Linear(hidden_dim, 1, rngs=rngs, kernel_init=orthogonal(1.0), bias_init=constant(0.0))
+            nnx.Linear(
+                hidden_dim,
+                1,
+                rngs=rngs,
+                kernel_init=orthogonal(1.0),
+                bias_init=constant(0.0),
+            ),
         )
 
     def __call__(self, x: jax.Array) -> tuple[distrax.Categorical, jax.Array]:
         logits = self.policy(x)
         return distrax.Categorical(logits), self.value(x)
 
+
 @nnx.jit
-def get_action_logprobs_value(model: Model, obs: jax.Array, key: jax.Array) -> tuple[jax.Array, jax.Array, jax.Array]:
+def get_action_logprobs_value(
+    model: Model, obs: jax.Array, key: jax.Array
+) -> tuple[jax.Array, jax.Array, jax.Array]:
     pi, value = model(obs)
     action = pi.sample(seed=key)
     return action, pi.log_prob(action), value
 
+
 @nnx.jit
-def get_action(model: Model, obs: jax.Array, key: jax.Array) -> jax.Array: return model(obs)[0].sample(seed=key)
+def get_action(model: Model, obs: jax.Array, key: jax.Array) -> jax.Array:
+    return model(obs)[0].sample(seed=key)
 
-# @nnx.jit
-def loss_fn(model, observations, actions, values, actions_log_probs, advantages, returns):
 
+@nnx.jit
+def loss_fn(
+    model, observations, actions, values, actions_log_probs, advantages, returns
+):
     observations = jax.lax.stop_gradient(observations)
     actions = jax.lax.stop_gradient(actions)
     values = jax.lax.stop_gradient(values)
@@ -66,17 +118,24 @@ def loss_fn(model, observations, actions, values, actions_log_probs, advantages,
     pi, value = model(observations)
     value = value.flatten()
 
-    prob_ratio = jnp.exp(pi.log_prob(actions) - actions_log_probs) # = p_new(a_t) / p_old(a_t) because of log rules log(a/b) = log(a) - log(b)
+    prob_ratio = jnp.exp(
+        pi.log_prob(actions) - actions_log_probs
+    )  # = p_new(a_t) / p_old(a_t) because of log rules log(a/b) = log(a) - log(b)
 
-    policy_loss = -jnp.mean(jnp.minimum( # loss is -L_CLIP because we want to maximize L_CLIP but using gradient descent
-        prob_ratio * advantages, jnp.clip(prob_ratio, min=1.0-EPS, max=1.0+EPS) * advantages
-    ))
+    policy_loss = -jnp.mean(
+        jnp.minimum(  # loss is -L_CLIP because we want to maximize L_CLIP but using gradient descent
+            prob_ratio * advantages,
+            jnp.clip(prob_ratio, min=1.0 - EPS, max=1.0 + EPS) * advantages,
+        )
+    )
     value_loss = jnp.mean(jnp.square(value - returns))
     entropy_loss = -jnp.mean(pi.entropy())
-    return policy_loss + 0.5 * value_loss + 0.0 * entropy_loss
+    return policy_loss + 0.5 * value_loss + 0.001 * entropy_loss
+
 
 def calculate_gae_returns(rewards, values, dones, last_value):
-    """ adapted from https://github.com/luchris429/purejaxrl/blob/main/purejaxrl/ppo.py#L142 """
+    """adapted from https://github.com/luchris429/purejaxrl/blob/main/purejaxrl/ppo.py#L142"""
+
     def _get_advantages(gae_and_next_value, reward_value_done):
         gae, next_value = gae_and_next_value
         reward, value, done = reward_value_done
@@ -88,18 +147,22 @@ def calculate_gae_returns(rewards, values, dones, last_value):
         _get_advantages,
         (jnp.zeros_like(last_value), last_value),
         (rewards, values, dones),
-        reverse=True, # scanning the trajectory batch in reverse order
+        reverse=True,  # scanning the trajectory batch in reverse order
     )
-    return advantages, advantages + values # advantages + values = returns
+    return advantages, advantages + values  # advantages + values = returns
+
 
 SEED = 7
 key = jax.random.key(SEED)
 env = gym.make("LunarLander-v3")
-model = Model(8, 128, 4) # lunar lander has obs (8,) and action(1,) in [0,3] range
-optimizer = nnx.Optimizer(model,optax.chain(
-  optax.clip(0.5),
-  optax.adamw(1e-3),
-))  # reference sharing
+model = Model(8, 128, 4)  # lunar lander has obs (8,) and action(1,) in [0,3] range
+optimizer = nnx.Optimizer(
+    model,
+    optax.chain(
+        optax.clip_by_global_norm(0.5),
+        optax.adamw(1e-3),
+    ),
+)  # reference sharing
 
 value_and_grad_loss_fn = nnx.value_and_grad(loss_fn)
 
@@ -127,14 +190,16 @@ for iteration in range(ITERATIONS):
         actions.append(action)
         actions_log_probs.append(log_probs)
         observation, reward, terminated, truncated, info = env.step(action)
-        rewards.append(reward)
+        rewards.append(float(reward))
 
         dones.append(terminated or truncated)
         if terminated or truncated:
-            done_rewards.append(reward)
+            done_rewards.append(float(reward))
             observation, _ = env.reset()
     key, subkey = jax.random.split(key)
-    last_value = get_action_logprobs_value(model, observation, subkey)[2].item() # one more required for gae's deltas
+    last_value = get_action_logprobs_value(model, observation, subkey)[
+        2
+    ].item()  # one more required for gae's deltas
 
     # Compute advantage estimates Â_1,..., Â_T
     # deltas: list[float] = [r + DISCOUNT_FACTOR * (1 - d) * nv - v for r, v, nv, d in zip(rewards, value_estimates[:-1], value_estimates[1:], dones)]
@@ -155,7 +220,9 @@ for iteration in range(ITERATIONS):
     # advantages = jnp.array(advantages)
     # returns = advantages + value_estimates
 
-    advantages, returns = calculate_gae_returns(rewards, value_estimates, dones, last_value)
+    advantages, returns = calculate_gae_returns(
+        rewards, value_estimates, dones, last_value
+    )
 
     print("iter mean reward", jnp.mean(rewards))
     print(f"done rewards {done_rewards}")
@@ -165,19 +232,30 @@ for iteration in range(ITERATIONS):
         # print(f"{epoch=}")
         for batch in range(len(observations) // BATCH_SIZE):
             key, _key = jax.random.split(key)
-            batch_idxs = jax.random.randint(_key, shape=(BATCH_SIZE,), minval=0, maxval=len(observations))
-            loss, grads = value_and_grad_loss_fn(model, observations[batch_idxs], actions[batch_idxs], value_estimates[batch_idxs], actions_log_probs[batch_idxs], advantages[batch_idxs], returns[batch_idxs])
+            batch_idxs = jax.random.randint(
+                _key, shape=(BATCH_SIZE,), minval=0, maxval=len(observations)
+            )
+            loss, grads = value_and_grad_loss_fn(
+                model,
+                observations[batch_idxs],
+                actions[batch_idxs],
+                value_estimates[batch_idxs],
+                actions_log_probs[batch_idxs],
+                advantages[batch_idxs],
+                returns[batch_idxs],
+            )
             optimizer.update(grads)  # inplace updates
 env.close()
 
-# # EVAL
-# env = gym.make("LunarLander-v3", render_mode="human")
+# EVAL
+env = gym.make("LunarLander-v3", render_mode="rgb_array")
+env = gym.wrappers.RecordVideo(env, video_folder="./videos", disable_logger=True)
+observation, _ = env.reset(seed=42)
+for _ in range(1000):
+    key, subkey = jax.random.split(key)
+    action = get_action(model, observation, subkey).item()
+    observation, reward, terminated, truncated, _ = env.step(action)
+    if terminated or truncated:
+        observation, _ = env.reset()
 
-# observation, _ = env.reset(seed=42)
-# for _ in range(1000):
-#     key, subkey = jax.random.split(key)
-#     action = get_action(model, observation, subkey).item()
-#     observation, reward, terminated, truncated, _ = env.step(action)
-#     if terminated or truncated: observation, _ = env.reset()
-
-# env.close()
+env.close()
